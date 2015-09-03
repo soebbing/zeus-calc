cdad29b7eda23078b44254a178ef91193295ebdc
Hendrik Söbbing <h.soebbing@shopmacher.de>
2015-09-03 08:37:26 +0200

    - React auf 0.13.3 aktualisiert - classnames-Modul integriert - bower-Module aus Repo entfernt

--- build.js
+++ build.js
@@ -6,6 +6,7 @@
     paths: {
         requirejs: 'vendors/requirejs/require',
         react:     'vendors/react/react-with-addons',
+        classnames:     'vendors/classnames/dedupe',
         text:           'vendors/requirejs-text/text',
         JSXTransformer: 'vendors/jsx-requirejs-plugin/js/JSXTransformer',
         jsx:            'vendors/jsx-requirejs-plugin/js/jsx',
@@ -17,5 +18,5 @@
     optimize: 'uglify',
     removeCombined: true,
     findNestedDependencies: true,
-    include: ['requirejs', 'react', 'app', 'src/services/Time']
+    include: ['requirejs', 'react', 'app', 'classnames', 'src/services/Time']
 })
